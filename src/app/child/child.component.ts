ff83e4a44bfdc39bce902fe8a87231437481905c
bala <sr.bala.kkl@gmail.com>
2025-06-09 23:14:21 +0530

    NEW:ContentChildren in angular 19

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import {Component, ContentChild, ElementRef, OnInit} from '@angular/core';
+import {AfterContentInit, Component, ContentChild, ContentChildren, ElementRef, OnInit, QueryList} from '@angular/core';
 import {GrandChildComponent} from "../grand-child/grand-child.component";
 
 @Component({
@@ -6,14 +6,27 @@ import {GrandChildComponent} from "../grand-child/grand-child.component";
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit{
-  @ContentChild('childTempRef',{static:true}) paraElement!:ElementRef;
-  @ContentChild(GrandChildComponent,{static:true}) gcElement!:GrandChildComponent;
+export class ChildComponent implements OnInit, AfterContentInit {
+  @ContentChild('childTempRef', {static: true}) paraElement!: ElementRef;
+  @ContentChild(GrandChildComponent, {static: true}) gcElement!: GrandChildComponent;
+
+
+  @ContentChildren('childTempRef') refChildrenElements!: QueryList<ElementRef>;
+
+  @ContentChildren(GrandChildComponent) grandChildrenElement!: QueryList<any>;
 
   ngOnInit() {
-    console.log('The project content value ==',this.paraElement.nativeElement.innerText)
-    console.log('The project content value ==',this.paraElement)
-    console.log('Gc element',this.gcElement.name + ' --- From parent component (child component)');
+    console.log('The project content value ==', this.paraElement.nativeElement.innerText)
+    console.log('The project content value ==', this.paraElement)
+    console.log('Gc element', this.gcElement.name + ' --- From parent component (child component)');
+  }
+
+  ngAfterContentInit(): void {
+    console.log(this.refChildrenElements)
+
+
+    console.log(this.grandChildrenElement)
+    console.log(this.grandChildrenElement.get(2).name)
   }
 
 }
